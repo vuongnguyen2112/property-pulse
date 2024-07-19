972ba18d8eb66fe676a242f91a50c11268eaf589
vuongnguyen2112 <a5ntvuong@gmail.com>
2024-07-19 19:36:00 +0700

    update code to deploy

--- middleware.js
+++ middleware.js
@@ -1,5 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/properties/add", "/profile", "/properties/saved", "/message"],
+  matcher: [
+    "/properties/add",
+    "/profile",
+    "/properties/saved",
+    "/message",
+    "/properties/:id/edit",
+  ],
 };
