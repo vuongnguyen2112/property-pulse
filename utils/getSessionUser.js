b4131654317bc46c814d40c5d25da3b2614d0b4b
vuongnguyen2112 <a5ntvuong@gmail.com>
2024-07-19 19:35:01 +0700

    remove codeto deploy

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -2,19 +2,14 @@ import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-      return null;
-    }
-
-    return {
-      user: session.user,
-      userId: session.user.id,
-    };
-  } catch (error) {
-    console.error(error.message);
+  if (!session || !session.user) {
     return null;
   }
+
+  return {
+    user: session.user,
+    userId: session.user.id,
+  };
 };
